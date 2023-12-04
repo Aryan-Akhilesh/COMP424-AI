0eb538288684c09ef8a9d59f13f0abb0a91c0109
Aryan-Akhilesh <aryanakhil99@gmail.com>
2023-12-04 01:15:08 -0500

    Initial commit

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,10 +1,11 @@
 # Student agent: Add your own agent here
 from agents.agent import Agent
 from store import register_agent
-import sys
+#import sys
 import numpy as np
 from copy import deepcopy
 import time
+from queue import PriorityQueue
 
 
 @register_agent("student_agent")
@@ -45,8 +46,76 @@ class StudentAgent(Agent):
         # so far when it nears 2 seconds.
         start_time = time.time()
         time_taken = time.time() - start_time
-        
+
+        steps_taken = 0
+        dist_bt_adversary = self.distance_to_adversary(my_pos, adv_pos)
+
+        possible_moves = self.possible_moves(chess_board, my_pos, adv_pos, steps_taken, max_step)
+
         print("My AI's turn took ", time_taken, "seconds.")
 
         # dummy return
         return my_pos, self.dir_map["u"]
+
+    # Heuristic function 1
+    # We are going to try to trap the opponent by moving closer to them
+    # We calculate the Manhattan distance for it since our agent cannot move diagonally
+    # Also because it is less computationally expensive than Euclidean
+    def distance_to_adversary(self, my_pos, adv_pos):
+        x1, y1 = my_pos
+        x2, y2 = adv_pos
+        shortest_distance = np.abs(x2-x1) + np.abs(y2-y1)
+        return shortest_distance
+
+    # Heuristic function 2
+    # We don't want to enter a box that has 3 walls
+    def num_walls(self, chess_board, my_pos):
+        num_walls = 0
+        x1, y1 = my_pos
+        for direction in self.dir_map.values():
+            if chess_board[x1, y1, direction]:
+                num_walls += 1
+        return num_walls
+
+    # Evaluation function
+    def eval(self, chess_board, my_pos, adv_pos):
+        h1 = self.distance_to_adversary(my_pos, adv_pos)
+        h2 = self.num_walls(chess_board, my_pos)
+        result = h2 + 0.7*h1
+        return result
+
+    def bfs(self, chess_board, my_pos, adv_pos, steps_taken, max_step):
+        frontier = self.find_frontier(chess_board, my_pos, adv_pos)
+        steps_taken += 1
+        visited = []
+        while steps_taken <= max_step:
+            new_pos = frontier.get()[1]
+            new_frontier = self.find_frontier(chess_board, new_pos, adv_pos)
+            # Condition to check if I have reached to a desired box, ie the opponent is in an adjacent box
+            steps_taken += 1
+
+    def find_frontier(self, chess_board, my_pos, adv_pos):
+        frontier = PriorityQueue()
+        x1, y1 = my_pos
+        for direction in self.dir_map.values():
+            if not chess_board[x1, y1, direction]:
+                if direction == 0 and (x1-1, y1) != adv_pos:
+                    value = self.eval(chess_board, (x1-1, y1), adv_pos)
+                    frontier.put((value, (x1-1, y1)))
+                elif direction == 1 and (x1, y1+1) != adv_pos:
+                    value = self.eval(chess_board, (x1, y1+1), adv_pos)
+                    frontier.put((value, (x1, y1+1)))
+                elif direction == 2 and (x1+1, y1) != adv_pos:
+                    value = self.eval(chess_board, (x1+1, y1), adv_pos)
+                    frontier.put((value, (x1+1, y1)))
+                elif direction == 3 and (x1,y1-1) != adv_pos:
+                    value = self.eval(chess_board, (x1, y1-1), adv_pos)
+                    frontier.put((value, (x1, y1-1)))
+
+        return frontier
+
+
+
+
+
+
